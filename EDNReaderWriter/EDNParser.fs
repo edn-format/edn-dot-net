bbbbac202648e448ae22b4ac720ec8b8c49d1b00
mperrotta@thortech-solutions.com <mark_perrotta@hotmail.com>
2012-12-10 10:40:28 -0500

    Performance improvement: removed regex.

--- EDNReaderWriter/EDNParser.fs
+++ EDNReaderWriter/EDNParser.fs
@@ -78,25 +78,42 @@ module EDNParser =
         pchar ';' >>. 
         restOfLine false |>> (fun s -> EDNComment(s))
 
-    let internal isAlphaChar = 
-        let regex = new Regex("[a-zA-Z]")
-        fun (c : char) ->
-            regex.IsMatch(string(c))
+    let internal isAlphaChar c = 
+        match c with
+        | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm'  
+        | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' 
+        | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M'  
+        | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' -> true
+        | _ -> false
 
-    let internal isValidInnerSymbolChar = 
-        let regex = new Regex("[a-zA-Z0-9#:.*!?$%&=+_-]")
-        fun (c : char) ->
-            regex.IsMatch(string(c))
+    let internal isValidInnerSymbolChar c = 
+        match c with
+        | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm'  
+        | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' 
+        | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M'  
+        | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' 
+        | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' 
+        | '#' | ':' | '.' | '*' | '!' | '?' | '$' | '%' | '&' | '=' | '+' | '_' | '-' -> true
+        | _ -> false
+        
+    let internal isValidFirstSymbolChar c = 
+        match c with
+        | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm'  
+        | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' 
+        | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M'  
+        | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' 
+        | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' 
+        | '.' | '*' | '!' | '?' | '$' | '%' | '&' | '=' | '+' | '_' | '-' -> true
+        | _ -> false
 
-    let internal isValidFirstSymbolChar = 
-        let regex = new Regex("[a-zA-Z.*!?$%&=+_-]")
-        fun (c : char) ->
-            regex.IsMatch(string(c))
-
-    let internal isNonNumericSymbolChar = 
-        let regex = new Regex("[a-zA-Z#:.*!?$%&=+_-]")
-        fun (c : char) ->
-            regex.IsMatch(string(c))
+    let internal isNonNumericSymbolChar c = 
+        match c with
+        | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm'  
+        | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' 
+        | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M'  
+        | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' 
+        | '#' | ':' | '.' | '*' | '!' | '?' | '$' | '%' | '&' | '=' | '+' | '_' | '-' -> true
+        | _ -> false
 
     let internal parseSymbolPart : Parser<string, unit> = 
         skipWhiteSpace >>.
